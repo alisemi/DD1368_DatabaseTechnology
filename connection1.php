3b44607f71dec5ac7723ab315aeac83be317ca83
Doren Calliku <dcalliku@gmail.com>
2018-03-05 18:03:31 +0100

    Correct version uploaded.
    Check 2 constrictions:
    Deleting user who was had created a meeting.
    Constraining time when choosing slots so that it will not choose time in the past.

---
+++ connection1.php
@@ -0,0 +1,170 @@
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="description" content=" DD1368, Spring2018, KTH, Stockholm">
+      <meta name="keywords"    content="HTML,CSS,PHP">
+      <meta name="author"      content="Doren Calliku, Ali Semi Yenimol">
+      <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+      <title>
+         Insert Meeting: Choose User.
+      </title>
+      <style type="text/css" media="screen">
+         * {
+         margin:   3px 3px 3px 3px;
+         padding:  3px 3px 3px 3px;
+         }
+         body, html {
+         padding: 3px 3px 3px 3px;
+         background-color: #D8DBE2;
+         font-family: Verdana, sans-serif;
+         font-size: 11pt;
+         text-align: center;
+         }
+         div.main_page {
+         position: relative;
+         display: table;
+         width: 800px;
+         margin-bottom: 3px;
+         margin-left: auto;
+         margin-right: auto;
+         padding: 0px 0px 0px 0px;
+         border-width: 2px;
+         border-color: #212738;
+         border-style: solid;
+         background-color: #FFFFFF;
+         text-align: relative;
+         }
+         div.page_header {
+         height: 100%;
+         width: 100%;
+         background-color: #F5F6F7;
+         }
+         div.page_header span {
+         margin: 15px 0px 0px 50px;
+         font-size: 180%;
+         font-weight: bold;
+         }
+         div.page_header img {
+         margin: 3px 0px 0px 40px;
+         border: 0px 0px 0px;
+         }
+         div.content_section {
+         margin: 3px 3px 3px 3px;
+         background-color: #FFFFFF;
+         text-align: left;
+         }
+         div.section_header {
+         padding: 3px 6px 3px 6px;
+         background-color: #8E9CB2;
+         color: #FFFFFF;
+         font-weight: bold;
+         font-size: 112%;
+         text-align: center;
+         }
+         .floating_element {
+         display: inline-block;
+         position: relative;
+         float: center;
+         }
+         .img {
+         width:100%;
+         }
+         .radioLeft {
+         display: block;
+         text-align: left;
+         float: left;
+         }
+         #formItem label {
+         display: block;
+         text-align: center;
+         line-height: 150%;
+         font-size: .85em;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="main_page">
+         <div class="section_header">
+            Choose staff and time slots.<br> 
+         </div>
+         <form action="welcome.php" method="POST" style="vertical-align: lef margin: 0px;">
+            <?php
+               //Connection to database
+               $servername = "localhost";
+               $username = "root";
+               $password = "Alumni2019";
+               // Create connection
+               
+               $conn = new PDO("mysql:host=$servername;dbname=dd1368", $username, $password);
+               $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               
+               $sql = "SELECT user_id, username, name, surname, position FROM User INNER JOIN Staff ON staff_id = user_id"; 
+               $stmt = $conn->prepare($sql);
+               $stmt->execute();
+               $result = $stmt->fetchAll();
+               ?>
+            <div style="text-align:left">
+               <?php
+                  // Print everything
+                  foreach($result as $row) {
+                  	echo "<li>" ;
+                  	echo $row['user_id'].". ",$row['username']." (",$row['name']." ",$row['surname'].") who works as ",$row['position']. ".";
+                  	echo "</li>";
+                  }
+   		
+                  ?>
+            </div>
+
+            <!-- Choose time slot and date-->
+            <br>
+
+
+            <div class="section_header">
+               <label for="date">
+               Meeting Date : 
+               </label>
+<!-- To be added constriction for dateInput> dateNow. In php it is written in the following form.date("Y/m/d")-->
+               <input id="date" name = "date" type="date" value="2018-25-02" required/> 
+               </br></br>
+               <hr>
+               <label for="start_time">
+               Meeting Start Time : 
+               </label>
+               <input id="start_time" name = "start_time" type="time" value="05:00:00" required/> 
+               </br></br>
+               <hr>
+               <label for="end_time"> 
+               Meeting End Time : 
+               </label>
+               <input id="end_time"   name = "end_time"   type="time" value="06:00:00" required/>	
+               </br></br>
+            </div>
+            <?php
+               if(isset($_POST['date_name'])) {
+                 echo "selected size: ".htmlspecialchars($_POST['date']);
+               }
+               if(isset($_POST['start_time'])) {
+                 echo "selected size: ".htmlspecialchars($_POST['start_time']);
+               }
+               if(isset($_POST['end_time'])) {
+                 echo "selected size: ".htmlspecialchars($_POST['end_time']);
+               }
+               if(isset($_POST['select'])) {
+                 echo "selected size: ".htmlspecialchars($_POST['select']);
+               }
+                $conn = null;
+               ?> 
+            <select name='select' class="floating_element">
+               <?php foreach ($result as $row): ?>
+               <option><?=$row['user_id']#.". ",$row['username']." (",$row['name']." ",$row['surname'].") who works in ",$row['position']. "."?></option>
+               <?php endforeach ?>
+            </select>
+            <input type="submit" name="user_info" value="Book Meeting" style="color:blue"/>
+         </form>
+      </div>
+   </body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,252 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <meta charset="UTF-8">
+    <meta name="description" content=" DD1368, Spring2018, KTH, Stockholm">
+    <meta name="keywords"    content="HTML,CSS,PHP">
+    <meta name="author"      content="Doren Calliku, Ali Semi Yenimol">
+    <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+    <title>DD1368 : DATABASE TECHNOLOGY </title>
+    <style type="text/css" media="screen">
+  * {
+    margin: 0px 0px 0px 0px;
+    padding: 0px 0px 0px 0px;
+  }
+
+  body, html {
+    padding: 3px 3px 3px 3px;
+
+    background-color: #D8DBE2;
+
+    font-family: Verdana, sans-serif;
+    font-size: 11pt;
+    text-align: center;
+  }
+
+  div.main_page {
+    position: relative;
+    display: table;
+
+    width: 800px;
+
+    margin-bottom: 3px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0px 0px 0px 0px;
+
+    border-width: 2px;
+    border-color: #212738;
+    border-style: solid;
+
+    background-color: #FFFFFF;
+
+    text-align: center;
+  }
+
+  div.page_header {
+    height: 99px;
+    width: 100%;
+
+    background-color: #F5F6F7;
+  }
+
+  div.page_header span {
+    margin: 15px 0px 0px 50px;
+
+    font-size: 180%;
+    font-weight: bold;
+  }
+
+  div.page_header img {
+    margin: 3px 0px 0px 40px;
+
+    border: 0px 0px 0px;
+  }
+
+  div.table_of_contents {
+    clear: left;
+
+    min-width: 200px;
+
+    margin: 3px 3px 3px 3px;
+
+    background-color: #FFFFFF;
+
+    text-align: left;
+  }
+
+  div.table_of_contents_item {
+    clear: left;
+
+    width: 100%;
+
+    margin: 4px 0px 0px 0px;
+
+    background-color: #FFFFFF;
+
+    color: #000000;
+    text-align: left;
+  }
+
+  div.table_of_contents_item a {
+    margin: 6px 0px 0px 6px;
+  }
+
+  div.content_section {
+    margin: 3px 3px 3px 3px;
+
+    background-color: #FFFFFF;
+
+    text-align: left;
+  }
+
+  div.content_section_text {
+    padding: 4px 8px 4px 8px;
+
+    color: #000000;
+    font-size: 100%;
+  }
+
+  div.content_section_text pre {
+    margin: 8px 0px 8px 0px;
+    padding: 8px 8px 8px 8px;
+
+    border-width: 1px;
+    border-style: dotted;
+    border-color: #000000;
+
+    background-color: #F5F6F7;
+
+    font-style: italic;
+  }
+
+  div.content_section_text p {
+    margin-bottom: 6px;
+  }
+
+  div.content_section_text ul, div.content_section_text li {
+    padding: 4px 8px 4px 16px;
+  }
+
+  div.section_header {
+    padding: 3px 6px 3px 6px;
+
+    background-color: #8E9CB2;
+
+    color: #FFFFFF;
+    font-weight: bold;
+    font-size: 112%;
+    text-align: center;
+  }
+
+  div.section_header_red {
+    background-color: #CD214F;
+  }
+
+  div.section_header_grey {
+    background-color: #9F9386;
+  }
+
+  .floating_element {
+    display: inline-block;
+    position: relative;
+    float: center;
+  }
+
+  div.table_of_contents_item a,
+  div.content_section_text a {
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  div.table_of_contents_item a:link,
+  div.table_of_contents_item a:visited,
+  div.table_of_contents_item a:active {
+    color: #000000;
+  }
+
+  div.table_of_contents_item a:hover {
+    background-color: #000000;
+
+    color: #FFFFFF;
+  }
+
+  div.content_section_text a:link,
+  div.content_section_text a:visited,
+   div.content_section_text a:active {
+    background-color: #DCDFE6;
+
+    color: #000000;
+  }
+
+  div.content_section_text a:hover {
+    background-color: #000000;
+
+    color: #DCDFE6;
+  }
+
+  
+  div.validator {
+  }
+img {
+    width:100%;
+}
+    </style>
+
+  </head>
+  <body>
+    <div class="main_page">
+      <div class="page_header floating_element">
+        <span class="floating_element"> DD1368: DATABASE TECHNOLOGY </span>
+      </div>
+
+      <div class="content_section floating_element">
+        <div class="section_header section_header_red">
+          <div id="about"></div>
+            These are the links to the tasks. 
+          </div>
+        <div class="content_section_text">
+          <p>
+                Hey ! </br>
+		This page is an introductory page for the Lab 2 of the Database course.</br>
+		Mainly we introduce with two functions: </br>
+		<ul style="list-style-type:circle">
+  		<li> <a href="connection1.php">Insert Meeting</a></li>
+  		<li> <a href="deletion1.php">Delete Meeting</a></li>
+		</ul> 	
+		Also, in order to check the present state of the database we can check the status of the information.
+		<ul style="list-style-type:circle">
+		<li> <a href="status.php">Present Status of the dd1368 database.</a></li>
+		</ul>
+          </p>
+
+        </div>
+
+        <div class="section_header">
+          <div id="changes">
+	  </div>
+                These are the diagrams of modelling the simple database.
+        </div>
+
+        <div class="content_section_text">
+          <p>
+		
+		<ul style="list-style-type:circle">
+  		<li> This is the Entity Relationship Diagram. You can also find it in github <a href=//github.com/DorenCalliku/DD1368_DatabaseTechnology/blob/master/diagram/er.png> here </a>. </li>
+  		<li> This is the Relational Diagram. You can also find it in github <a href=//github.com/DorenCalliku/DD1368_DatabaseTechnology/blob/master/diagram/lab2-relational.png>here</a>. </li>
+		</ul>  
+		</br> </br> <hr>
+                 <img src="er.jpg"     alt="Entity Relationship Diagram" > 
+		</br> </br> <hr>
+		 <img src="lab2-relational.jpg" alt="Relational Diagram" >
+          </p>
+        </div>
+
+
+
+
+      </div>
+    </div>
+
+  </body>
+</html>
+
