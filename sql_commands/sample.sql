bd270a23ce505e7f0951f944ba0425119f0a91da
Doren Calliku <dcalliku@gmail.com>
2018-03-05 18:09:26 +0100

    Changed meetings, booking, resources, payments.
    This was done in order to have a more controllable database. 
    Now one can understand better the structure of the information.

--- sql_commands/sample.sql
+++ sql_commands/sample.sql
@@ -1,39 +1,75 @@
 
 /* User */
 
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Lenore","DTN33UAZ8KD","Debra, Ivan","Moran","A Feugiat Tellus Corp."),("Madonna","PYL63HXW6JU","John, Sylvester","Dorsey","Magna Tellus LLC"),("Christen","EOX00TTO6TJ","Ayanna, Hedda","Hicks","A Ltd"),("Jasper","SAE50BQH1FQ","Willa, Blaze","Workman","Enim Mauris Industries"),("Jillian","GZL03JER0OB","David, Autumn","Knapp","Ipsum Institute"),("Rahim","KOZ58JYW0QV","Cheryl, Lenore","Salas","Erat Institute"),("Angela","GIZ49IIC8TO","Nora, Oprah","Suarez","Integer Industries"),("Regan","SVJ59PFY3SF","Quinn, Phelan","Cash","Dis Consulting"),("Robin","KRX74UHT2VW","Clarke, Reece","Whitaker","Orci Limited"),("Isaiah","XJV34EKN5VL","Abel, Grady","Snow","In Lobortis Institute");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Salvador","JQJ99ILL5ZN","Noble, Emmanuel","Oneil","Vestibulum Limited"),("Jayme","ADX50APF9QS","Regina, Camille","Freeman","Primis In Faucibus Foundation"),("Finn","CVE72YYC0DN","Amela, Ray","Fuller","Interdum Enim Non Corp."),("Lacota","EUI09FHV9JI","Irma, Nero","Kim","Elit Pellentesque A Corp."),("Norman","HRJ83MCW7YD","Talon, Ivor","Hoffman","Aliquam Limited"),("Macon","DOP88TEG2WG","Hermione, Kristen","Tanner","Accumsan Ltd"),("Acton","GEJ67LIN4NE","Maryam, Emi","Rich","Non Egestas A Inc."),("Kitra","JAQ47QKZ1YR","Ferdinand, Damon","Ware","Pellentesque Eget Foundation"),("Edan","JSF35EDU7YT","Alana, Emerald","Key","Tincidunt Associates"),("Chancellor","WDV65SCV9WR","Cherokee, Cadman","Dillon","Nam Nulla Foundation");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Juliet","SHF82XEY5YD","Eleanor, Ulysses","Santos","Mauris Consulting"),("Griffith","ZFT15WWC6WN","Reece, Logan","Tate","Volutpat Ornare Facilisis Associates"),("Whoopi","ATN44CXK3DZ","Hu, Phelan","Jensen","Est Institute"),("Jesse","HRJ93MAB7EH","Rashad, Laura","Harrington","Non Massa Consulting"),("Calvin","XHP50OSL8PS","Noah, Aquila","Delgado","Pharetra Quisque Inc."),("Mufutau","NMI85AWR9BP","Robin, Marvin","Gates","Vehicula Risus Nulla Foundation"),("Selma","VVQ03JGP0ZS","Carter, Brielle","Heath","Nec Diam Inc."),("Whoopi","QVL01ZRA8XS","Allistair, Curran","Thompson","At Velit Pellentesque Corp."),("Daphne","UEF94TUF4VN","Dylan, Thomas","Levine","Blandit Corporation"),("Mona","WIB55UPX4EL","Gray, Sheila","Palmer","Augue Porttitor Interdum PC");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Adrian","OQZ39JCD8TS","Oliver, Sydnee","Franks","Lacinia Inc."),("Natalie","LQD48GWH2VE","Faith, Aurora","Bolton","Rutrum Urna Ltd"),("Nero","GYL34CUE1HM","Nicole, Cheryl","Thompson","Ipsum Dolor Sit Institute"),("Orson","REZ28EBB3VL","Todd, Aretha","Herring","Nulla Corp."),("Katelyn","XER14AAV0LZ","Jeanette, Basil","Chavez","Velit Consulting"),("Jerome","EOP51DPN2TX","Shelly, Dai","Gates","Proin Ultrices Duis Corp."),("Rose","ZJM57OJA3DX","Stacy, Colt","Morgan","Nunc Interdum Incorporated"),("Bevis","IJH43WMA2SA","Xanthus, Edward","Drake","Vel Nisl Company"),("Kamal","KDO69LHJ0CH","Demetria, Elizabeth","Sellers","Lorem Tristique Aliquet LLC"),("Kiayada","OJM95GXN8YG","Haviva, Randall","Orr","Ac Mattis Foundation");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Paul","PSU28LBL2ID","Ginger, Patrick","Ramirez","Ultrices Corporation"),("Autumn","VPX97RDC0OW","Debra, Hiroko","Ruiz","Eu Nulla At PC"),("Ivana","MXY99BDD3EE","Jameson, Sara","Bird","Arcu Et Incorporated"),("Gil","OLV59UMM7DL","Hedda, Daniel","Cox","Auctor Non Company"),("Hedda","HBP48TJC2OM","Carissa, Petra","Fernandez","Dolor Elit Pellentesque Ltd"),("Quintessa","BXR35OGQ2YM","Jocelyn, Prescott","Bowman","Vestibulum Accumsan Neque LLP"),("Kasper","UEU84AZA3UM","Frances, Aristotle","Galloway","Lorem Luctus LLP"),("Charity","KJK11ALD2GH","Upton, Dakota","Combs","Augue Porttitor Incorporated"),("Xander","LQZ86CRU1LD","Galena, Jael","Morales","Sollicitudin Corp."),("Inga","WWH54TWZ5SM","Tiger, Kylie","Prince","Per Conubia Nostra Inc.");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Owen","XBA01UKK4CR","Camden, Yardley","Sanchez","Tristique Ac Eleifend Consulting"),("Rylee","SZE54FLZ8WW","Brock, Sharon","Juarez","In Consequat Associates"),("Armando","PAN34KIZ4IL","Fallon, Conan","Bentley","Penatibus Et Magnis Institute"),("Stephen","HVK40WFN4SJ","May, Driscoll","Sweeney","Porttitor Tellus Non Limited"),("Plato","AEI57XSB3HB","Madeson, Lewis","Pennington","Lorem Associates"),("Hadley","QRP74UYV8VK","Malik, Evan","Dominguez","Libero Company"),("Wayne","PFH74RQG4PQ","Blaine, Travis","Hoover","Ligula Industries"),("Stephanie","JMY06KDK7RA","Mariko, Quemby","Bowman","Lobortis Consulting"),("Desiree","YYT79SOI1IR","Lucian, Lunea","Mccormick","At Lacus Consulting"),("Akeem","KAP42CCU3MX","Nina, Felix","Quinn","Aenean Gravida Nunc Limited");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Yvette","EBY40UCQ4SN","Simone, Daria","Gomez","Amet Ante Vivamus Corp."),("Vincent","SBG46BYP4WF","Herman, Scarlett","Hahn","Id LLC"),("Chava","TLP32IFX7CY","Leslie, Sebastian","Miles","Vitae Erat Vel Industries"),("Colin","WTI58ANB2PF","Alea, Giselle","Moss","Eu Enim Etiam Associates"),("Joel","CDG89TKD5EV","Odessa, Avram","Finley","Purus Corp."),("Unity","SYX58XYE4YT","Solomon, Bryar","Saunders","Lacus Cras Inc."),("Remedios","POG79CYV4CY","Lee, Selma","Rosario","Enim Gravida Sit Associates"),("Flynn","NQY57FGJ3EJ","Sierra, Helen","Sheppard","Pellentesque Industries"),("Jerome","HNE99WOO8KO","Jermaine, Tanya","Hobbs","Quam Vel Institute"),("Keane","PON97SRC1RC","Leonard, Tallulah","Hurst","Ultricies Foundation");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Jared","BGP06OWU7ZR","Tiger, Joy","Justice","Lorem Corporation"),("Katelyn","ASG58SQP7BK","Molly, Mariam","Reynolds","Odio Aliquam Vulputate Company"),("Amity","RBG29HJQ7SJ","Stella, Caldwell","Small","A Scelerisque Corporation"),("Martena","DCH13RZB7AB","Kelsie, Iris","Boyd","Sed Diam Lorem Limited"),("Brooke","CNK39ZUI9PX","Nelle, Dylan","Molina","Vitae Limited"),("Maite","KOR70VNT3HW","Giacomo, Keaton","Day","Lorem Corp."),("Richard","EGW34MFL0DC","Katelyn, Wallace","Harris","Nec Corp."),("Henry","AUY00LTE8GF","Sonya, Quinlan","Lester","Porttitor Corp."),("Veda","DBV78REL9PO","Abbot, Mannix","Little","Feugiat Foundation"),("Tucker","WFC25URT3TL","Anthony, Elizabeth","Hill","Erat Vivamus Nisi Industries");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Mikayla","ESM29CRX2UG","Xenos, Harlan","Bradshaw","Consectetuer Ipsum Nunc Foundation"),("Sonia","ZNN81ZSJ9XK","Jemima, Fleur","Steele","Erat Volutpat Nulla Company"),("Alvin","AQD24AYR0DE","Keith, Anne","Hess","Venenatis LLC"),("Celeste","JXI44BPM8OH","Rama, Kyla","Sutton","Scelerisque LLC"),("Ria","OUW26JUN1XA","Raja, MacKenzie","Ellison","Nisl Arcu Iaculis LLP"),("Candice","JQB46NBC8BR","Kaye, Karyn","Mercado","Congue A Corp."),("Hector","OIY65JJK4XJ","Shafira, Jonah","Burgess","Interdum Curabitur Dictum Limited"),("Kareem","ZYQ91NAJ7AN","Amity, Guy","Walsh","Vestibulum Mauris Magna Corp."),("Yvette","OZZ31OLF0KN","Clementine, Marshall","Robles","Nisl Industries"),("Kenyon","QMK76DMB5RX","Robert, Maggy","Hendrix","Morbi Neque Industries");
-INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`) VALUES ("Faith","TDI28XDF0HR","Zeus, Porter","Sanchez","Ligula Ltd"),("Ariel","HKK59QMQ9QY","Randall, Colin","Hendricks","Rutrum Urna Associates"),("Calista","XVF72SIF7ZL","Maxwell, Brandon","Poole","Ac Nulla In PC"),("Wyatt","FYW88WER7XO","Salvador, Madaline","Wooten","Orci Foundation"),("Erica","STC45RVU9ID","Rowan, Tana","Mclaughlin","Tellus Imperdiet Industries"),("Buckminster","ECS50GGO2EO","Holmes, Quynn","Kaufman","Mi Pede Ltd"),("Louis","ZMJ82VWV4CI","Rylee, Dominic","Walsh","Diam Dictum Sapien Company"),("Dexter","RKL61THP0RY","Quinn, Katelyn","Avila","Quis Institute"),("Beau","PAL27CDS3UI","Rigel, Jessamine","Reed","Sapien Limited"),("Shad","ZHP92NYC7DB","Ezra, Medge","Austin","Libero Corp.");
-
+INSERT INTO `User` (`username`,`password`,`name`,`surname`,`position`)
+VALUES 
+("Lenore","DTN33UAZ8KD","Ivan","Moran","worker"),
+("Madonna","PYL63HXW6JU","John, Sylvester","Dorsey","worker"),
+("Christen","EOX00TTO6TJ","Ayanna, Hedda","Hicks","leader"),
+("Jasper","SAE50BQH1FQ","Willa, Blaze","Workman","worker"),
+("Jillian","GZL03JER0OB","David, Autumn","Knapp","president"),
+("Rahim","KOZ58JYW0QV","Cheryl, Lenore","Salas","worker"),
+("Angela","GIZ49IIC8TO","Nora, Oprah","Suarez","worker"),
+("Regan","SVJ59PFY3SF","Quinn, Phelan","Cash","leader"),
+("Robin","KRX74UHT2VW","Clarke, Reece","Whitaker","vc.president"),
+("Isaiah","XJV34EKN5VL","Abel, Grady","Snow","worker"),
+("Salvador","JQJ99ILL5ZN","Noble, Emmanuel","Oneil","worker"),
+("Jayme","ADX50APF9QS","Regina, Camille","Freeman","worker"),
+("Finn", "CVE72YYC0DN",     "Amela, Ray", "Fuller","leader"),
+("Lacota","EUI09FHV9JI","Irma, Nero","Kim","worker"),
+("Norman","HRJ83MCW7YD","Talon, Ivor","Hoffman","worker"),
+("Macon","DOP88TEG2WG","Hermione, Kristen","Tanner","worker"),
+("Acton","GEJ67LIN4NE","Maryam, Emi","Rich","worker"),
+("Kitra","JAQ47QKZ1YR","Ferdinand, Damon","Ware","worker"),
+("Edan","JSF35EDU7YT","Alana, Emerald","Key","worker"),
+("Chancellor","WDV65SCV9WR","Cherokee, Cadman","Dillon","leader"),
+("Juliet","SHF82XEY5YD","Eleanor","Santos","Mauris"),
+("Griffith","ZFT15WWC6WN","Reece, Logan","Tate","Volutpat"),
+("Whoopi","ATN44CXK3DZ","Hu, Phelan","Jensen","Institute"),
+("Alo", "HRJ93MAB7EH","Rashad, Laura","Harrington",   "Massa"),
+("Alo2","XHP50OSL8PS", "Noah, Aquila",   "Delgado","Pharetra"),
+("Alo3","NMI85AWR9BP","Robin, Marvin","Gates","Foundation"),
+("Alo4","VVQ03JGP0ZS","Carter, Brielle","Heath","Nec"),
+("Alo5","QVL01ZRA8XS","Allistair, Curran","Thompson","Corp"),
+("Alo6","UEF94TUF4VN","Dylan, Thomas","Levine","Blandit Corporation"),
+("Alo7","UEF94TUF4V4","Problem","Alma","Alma Corp");
 
 /* Staff */
 
-INSERT INTO Staff (staff_id) SELECT user_id FROM User ORDER BY user_id ASC LIMIT 50;
+INSERT INTO Staff (staff_id) SELECT user_id FROM User ORDER BY user_id ASC LIMIT 20;
 
 /* Business_Partner */
 
-INSERT INTO Business_Partner (partner_id, represents) SELECT user_id, position FROM User ORDER BY user_id DESC LIMIT 20;
+INSERT INTO Business_Partner (partner_id, represents) SELECT user_id, position FROM User ORDER BY user_id DESC LIMIT 10;
 
 
 /* Team */
 
-INSERT INTO `Team` (`name`,`status`) VALUES ("Indianapolis",1),("Woodlands County",1),("Villafranca in Lunigiana",1),("King Township",1),("Morwell",1),("Lincoln",1),("Bünyan",1),("San Joaquín",1),("Harnoncourt",1),("Maiolati Spontini",1);
+INSERT INTO `Team` (`name`,`status`)
+VALUES 
+("Team1",1),
+("Team2",1),
+("Team3",1),
+("Team4",1),
+("Team5",1);
 
 /* Team_In */
-INSERT INTO Team_In (team_name, staff_id) VALUES
-("Indianapolis",1),("Woodlands County",2),("Villafranca in Lunigiana",3),("King Township",4),("Morwell",5),("Lincoln",6),("Bünyan",7),("San Joaquín",8),("Harnoncourt",9),("Maiolati Spontini",10), 
-("Indianapolis",11),("Woodlands County",12),("Villafranca in Lunigiana",13),("King Township",14),("Morwell",15),("Lincoln",16),("Bünyan",17),("San Joaquín",18),("Harnoncourt",19),("Maiolati Spontini",20),("Indianapolis",41),("Woodlands County",42),("Villafranca in Lunigiana",43),("King Township",44),("Morwell",45),("Lincoln",46),("Bünyan",47),("San Joaquín",48),("Harnoncourt",49),("Maiolati Spontini",50), ("Indianapolis",31),("Woodlands County",32),("Villafranca in Lunigiana",33),("King Township",34),("Morwell",35),("Lincoln",36),("Bünyan",37),("San Joaquín",38),("Harnoncourt",39),("Maiolati Spontini",40);
+INSERT INTO Team_In (team_name, staff_id) 
+VALUES
+ ("Team1",1), ("Team1",2), ("Team1",3), ("Team1",4),
+ ("Team2",5), ("Team2",6), ("Team2",7), ("Team2",8),
+ ("Team3",9),("Team3",10),("Team3",11),("Team3",12),
+("Team4",13),("Team4",14),("Team4",15),("Team4",16),
+("Team5",17),("Team5",18),("Team5",19),("Team5",20);
 
 /* Meeting */
 
-INSERT INTO `Meeting` (`creator_id`,`date`,`end_time`,`start_time`) VALUES (1,"2018-02-25","06:42:11","14:42:48"),(2,"2017-09-16","08:28:06","02:44:43"),(3,"2017-08-24","18:17:56","09:55:37"),(4,"2017-08-07","08:33:49","18:28:40");
+INSERT INTO `Meeting` (`creator_id`,`date`,`end_time`,`start_time`) 
+VALUES 
+(1,"2018-02-25","06:42:11","14:42:48"),
+(5,"2017-09-16","08:28:06","02:44:43"),
+(9,"2017-08-24","18:17:56","09:55:37"),
+(15,"2017-08-07","08:33:49","18:28:40");
 
 /* Participate */
 INSERT INTO Participate (meeting_id, user_id)
@@ -44,72 +80,74 @@ VALUES
 (1,4),
 (1,5),
 (1,6),
-(1,85),
-(1,90);
-
-INSERT INTO Participate (meeting_id, user_id)
-VALUES
 (2,11),
 (2,12),
 (2,13),
 (2,14),
 (2,15),
 (2,16),
-(2,86),
-(2,90);
-INSERT INTO Participate (meeting_id, user_id)
-VALUES
+(2,22),
 (3,1),
 (3,2),
 (3,3),
-(3,44),
-(3,45),
+(3,14),
+(3,4),
 (3,6),
-(3,85),
-(3,98);
-INSERT INTO Participate (meeting_id, user_id)
-VALUES
+(3,24),
+(3,21),
 (4,1),
-(4,32),
-(4,33),
-(4,34),
-(4,35),
-(4,36),
-(4,81),
-(4,95);
+(4,23);
 
 /* Meeting_Payment */
 INSERT INTO Meeting_Payment (meeting_id, team_name, amount, status)
-VALUES(1, "Indianapolis", 2000, 0),
-(2, "Woodlands County", 500, 1),
-(3, "Villafranca in Lunigiana", 4000, 1),
-(4, "King Township", 1000, 0);
+VALUES
+(1, "Team1", 120, 0),
+(2, "Team2", 150, 1),
+(3, "Team3", 140, 1),
+(4, "Team4", 110, 0);
 
 
 /* Available_Resource */
 
-INSERT INTO `Available_Resource` (`room_no`,`capacity`,`address`,`building_name`) VALUES (10,91,"P.O. Box 574, 1767 Nunc Ave","Palestine, State of"),(1,82,"6526 Fringilla Road","Romania"),(8,77,"Ap #226-956 Quisque Av.","Saint Vincent and The Grenadines"),(3,10,"819-3714 Odio Road","South Sudan"),(10,18,"Ap #120-6951 Sodales Street","Eritrea"),(5,72,"873-3090 Sagittis Road","Estonia"),(4,95,"P.O. Box 226, 7016 Mattis St.","Burkina Faso"),(3,16,"P.O. Box 973, 7797 Id St.","Croatia"),(3,14,"P.O. Box 847, 5511 Sem St.","Saint Barthélemy"),(8,63,"565-4373 Mauris Av.","New Caledonia");
-INSERT INTO `Available_Resource` (`room_no`,`capacity`,`address`,`building_name`) VALUES (9,46,"447-2977 Nunc Ave","Panama"),(2,56,"3829 At Av.","Namibia"),(5,98,"P.O. Box 458, 5479 Odio. Rd.","Bosnia and Herzegovina"),(3,54,"Ap #995-8521 Ullamcorper St.","Poland"),(9,10,"4300 Vestibulum. St.","Malawi"),(9,97,"4969 Ut Rd.","Eritrea"),(2,68,"921-2436 Odio St.","Germany"),(2,64,"Ap #471-5346 Nascetur Ave","Mauritius"),(2,70,"Ap #580-5850 Nunc St.","Thailand"),(1,94,"Ap #980-1600 Maecenas Av.","Wallis and Futuna");
+INSERT INTO `Available_Resource` (`room_no`,`capacity`,`address`,`building_name`) 
+VALUES 
+(1,10,"KTH, E building E1, Stockholm","Sweden"),
+(2,10,"KTH, E building E2, Stockholm","Sweden"),
+(3,10,"KTH, E building E3, Stockholm","Sweden"),
+(4,10,"KTH, E building E4, Stockholm","Sweden"),
+(5,10,"KTH, E building E5, Stockholm","Sweden"),
+(6,10,"KTH, E building E6, Stockholm","Sweden");
+INSERT INTO `Available_Resource` (`room_no`,`capacity`,`address`,`building_name`) 
+VALUES 
+(1,10,"KTH, D building D1, Stockholm","Sweden"),
+(2,20,"KTH, D building D2, Stockholm","Sweden"),
+(3,20,"KTH, D building D3, Stockholm","Sweden"),
+(4,20,"KTH, D building D4, Stockholm","Sweden"),
+(5,20,"KTH, D building D5, Stockholm","Sweden"),
+(6,20,"KTH, D building D6, Stockholm","Sweden");
 
 /* Booking */
 INSERT INTO Booking (meeting_id, resource_id)
-VALUES(1, 1),
+VALUES
+(1, 1),
 (2, 2),
 (3, 3),
 (4, 4);
 
 /* Facility */
 
-INSERT INTO `Facility` (`name`,`cost`) VALUES ("quis",334),("egestas",377),("tellus",384),("malesuada",437),("Duis",200),("est",236),("et",185),("id,",168),("dolor,",492),("arcu.",245);
+INSERT INTO `Facility` (`name`,`cost`) 
+VALUES ("tv",10),
+("board",20),
+("chairs",40),
+("tables",40);
 
 /* Facility_In */
-INSERT INTO Facility_In (facility_name, resource_id) VALUES 
-("quis",1),("egestas",2),("tellus",3),("malesuada",4),("Duis",5),("est",6),("et",7),("id,",8),("dolor,",9),("arcu.",10);
-
-INSERT INTO Facility_In (facility_name, resource_id) VALUES 
-("quis",11),("egestas",12),("tellus",13),("malesuada",14),("Duis",15),("est",16),("et",17),("id,",18),("dolor,",19),("arcu.",20);
-
-INSERT INTO Facility_In (facility_name, resource_id) VALUES 
-("quis",7),("egestas",8),("tellus",9),("malesuada",10),("Duis",11),("est",12),("et",13),("id,",14),("dolor,",15),("arcu.",16);
+INSERT INTO Facility_In (facility_name, resource_id) 
+VALUES 
+("tv",1),("tv",2),("tv",3),("tv",4),("tv",5),("tv",6),("tv",7),("tv",8),("tv",9),("tv",10),("tv",11),
+("board",2),("board",4),("board",6),("board",10),("board",12),("board",11),("board",9),
+("chairs",1),("chairs",2),("chairs",3),("chairs",4),("chairs",5),("chairs",6),("chairs",7),
+("tables",5),("tables",6),("tables",7),("tables",8),("tables",9),("tables",10),("tables",11);
 
 
